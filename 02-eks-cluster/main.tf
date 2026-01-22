ec5f9d1dee4ac481378355c6c06a7a1f3407e220
jlui70 <luiz7030@gmail.com>
2026-01-22 10:14:41 -0300

    fix: Adiciona provider kubernetes no main.tf
    - Provider kubernetes estava faltando, causando erro 'connection refused'
    - Terraform tentava conectar em localhost:80 ao invés do cluster EKS
    - Agora usa mesma config do provider helm (exec com aws eks get-token)
    - Resolve erro ao criar kubernetes_secret do ArgoCD

--- 02-eks-cluster/main.tf
+++ 02-eks-cluster/main.tf
@@ -36,3 +36,13 @@ provider "helm" {
     }
   }
 }
+
+provider "kubernetes" {
+  host                   = aws_eks_cluster.this.endpoint
+  cluster_ca_certificate = base64decode(aws_eks_cluster.this.certificate_authority[0].data)
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    args        = ["eks", "get-token", "--cluster-name", aws_eks_cluster.this.id]
+    command     = "aws"
+  }
+}
